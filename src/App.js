e8ff51429a37ad4644d8408bf774325046d2b245
hamada <hamadagh777@gmail.com>
2021-10-29 17:16:46 +0200

    moving on the y axis

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useEffect, useRef } from "react";
 import "./App.css";
+import { gsap } from "gsap";
 
 function App() {
+  const boxRef = useRef();
+
+  useEffect(() => {
+    gsap.to(boxRef.current, { y: 300, repeat: -1, yoyo: true });
+  }, []);
   return (
     <div className="App">
-      <h1>App</h1>
+      <div className="box" ref={boxRef}>
+        <span className="letters">h</span>
+        <span className="fade-letters">a</span>
+        <span className="letters">mada</span>
+        <span className="letters"> Gh</span>
+        <span className="fade-letters">a</span>
+        <span className="letters">nem</span>
+      </div>
     </div>
   );
 }
